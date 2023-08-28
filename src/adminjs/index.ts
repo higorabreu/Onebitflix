7051dc84da1c8847e3bb528b25dd44d4db6ce981
Higor Abreu <higorabreuf@gmail.com>
2023-08-27 23:50:16 -0300

    translations add

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,6 +3,9 @@ import AdminJSExpress  from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import { sequelize } from "../database";
 import { adminJsResources } from "./resources";
+import { User } from "../models";
+import bcrypt from "bcrypt"
+import { locale } from './locale'
 
 AdminJS.registerAdapter(AdminJSSequelize)
 
@@ -10,6 +13,7 @@ export const adminJs = new AdminJS ({
     databases: [sequelize],
     rootPath: "/admin",
 	resources: adminJsResources,
+	locale: locale,
     branding: {
         companyName: 'OneBitFlix',
         logo: '/onebitflix.svg',
@@ -34,4 +38,22 @@ export const adminJs = new AdminJS ({
     }
 });
 
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
+	authenticate: async (email, password) => {
+		const user = await User.findOne({ where: { email } })
+
+		if (user && user.role === 'admin') {
+			const matched = await bcrypt.compare(password, user.password)
+
+			if (matched) {
+				return user
+			}
+		}
+
+		return false
+	},
+	cookiePassword: 'senha-do-cookie'
+}, null, {
+	resave: false,
+	saveUninitialized: false
+})
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { sequelize } from '../database'
 import { DataTypes, Model, Optional } from 'sequelize'
+import bcrypt from 'bcrypt'
 
 export interface User {
   id: number
@@ -57,4 +58,12 @@ export const User = sequelize.define<UserInstance, User>('users', {
     allowNull: false,
     type: DataTypes.STRING
   }
+}, {
+hooks: {
+  beforeSave: async (user) => {
+    if (user.isNewRecord || user.changed('password')) {
+      user.password = await bcrypt.hash(user.password.toString(), 10);
+    }
+  }
+}
 })
