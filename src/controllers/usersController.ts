7d384b8fa56b60daa5173e074041bd18a74babce
Higor Abreu <higorabreuf@gmail.com>
2023-09-04 19:54:14 -0300

    update user password

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -15,7 +15,7 @@ export const usersController = {
         }
     },
 
-    // PUt /users/current
+    // PUT /users/current
     update: async (req: AuthenticatedRequest, res: Response) => {
         const { id } = req.user!
         const { firstName, lastName, phone, email, birth} = req.body
@@ -34,6 +34,28 @@ export const usersController = {
                 return res.status(400).json({ message: err.message })     
         }
     }
+},
+
+    // PUT /users/current/password
+    updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+        const user = req.user!
+        const { currentPassword, newPassword } = req.body
+
+    user.checkPassword(currentPassword, async (err, isSame) => {
+        try {
+            if (err) return res.status(400).json({message: err.message})
+            if (!isSame) return res.status(400).json({message: 'Senha Incorreta.'})
+    
+            await userService.updatePassword(user.id, newPassword)
+            return res.status(204).send()
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })     
+            }
+    
+        }
+    })
+
 },
 
 
