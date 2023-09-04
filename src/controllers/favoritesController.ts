100f792083e5bea760ffff896180d1b375529be3
Higor Abreu <higorabreuf@gmail.com>
2023-09-03 23:01:24 -0300

    favorites get

--- src/controllers/favoritesController.ts
+++ src/controllers/favoritesController.ts
@@ -16,5 +16,18 @@ export const favoritesController = {
                 return res.status(400).json({ message: err.message })       
             }
         }
+    },
+
+    // GET /favorites
+    index: async (req: AuthenticatedRequest, res: Response) => {
+        const userId = req.user!.id
+        try {
+            const favorites = await favoriteService.findByUserId(userId)
+            return res.json(favorites)
+        } catch (err) {
+            if (err instanceof Error) {
+                return res.status(400).json({ message: err.message })       
+            }
+        }
     }
 }
