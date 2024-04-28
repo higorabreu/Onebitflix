d65312926ceb986d3e8b054dd9d1d931551e7d13
Higor Abreu <131218640+higorabreu@users.noreply.github.com>
2024-04-28 17:14:45 -0300

    Update sequelizeCli.js

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -4,7 +4,7 @@ module.exports = {
         host: 'localhost',
         port: '5432',
         database: 'streaming_development',
-        username: 'postgres',
-        password: 'postgres'
+        username: 'streaming',
+        password: 'streaming'
     }
 }
