1c2ad808faeba98a42d63624e7318a635e4b79c5
Higor Abreu <higorabreuf@gmail.com>
2024-04-28 16:41:41 -0300

    adminjs removed

--- config/sequelizeCli.js
+++ config/sequelizeCli.js
@@ -3,8 +3,8 @@ module.exports = {
         dialect: 'postgres',
         host: 'localhost',
         port: '5432',
-        database: 'onebitflix_development',
-        username: 'onebitflix',
-        password: 'onebitflix'
+        database: 'streaming_development',
+        username: 'postgres',
+        password: 'postgres'
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from "express"
 import { sequelize } from "./database"
-import { adminJs, adminJsRouter } from "./adminjs"
 import { router } from "./routes"
 import cors from 'cors'
 
@@ -11,8 +10,6 @@ app.use(cors())
 app.use(express.static('public'))
 
 app.use(express.json())
-
-app.use(adminJs.options.rootPath, adminJsRouter)
 
 app.use(router)
 
